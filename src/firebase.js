80755a1405627ceba6891aface8d6b7a62be7ead
Timothy Joseph <163113544+1975Renken@users.noreply.github.com>
2024-12-26 23:48:41 -0600

    Add Firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "personality-insights.firebaseapp.com",
+  projectId: "personality-insights",
+  storageBucket: "personality-insights.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+export default app;
